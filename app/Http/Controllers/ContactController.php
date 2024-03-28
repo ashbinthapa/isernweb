dea92ace57713775c81476d7db131a7b8945040d
ashbinthapa <ashbinthapa2054@gmail.com>
2024-03-28 11:22:50 +0545

    contact form email and database working

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -1,11 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\ContactForm; // Import the ContactForm model
 
 use Illuminate\Http\Request;
 use App\Mail\ContactFormMail;
 use Illuminate\Support\Facades\Mail;
+use App\Models\ContactForm; // Import the ContactForm model
+
 
 class ContactController extends Controller
 {
@@ -22,12 +23,22 @@ class ContactController extends Controller
             'message' => 'required',
         ]);
 
-        $contact = new ContactForm();
+        /*this block of code can be replaced by */
+        /*$contact = new ContactForm();
         $contact->name = $request->name;
         $contact->email = $request->email;
         $contact->message = $request->message;
         $contact->save();
+        */    
+        /*replaced by this */
+        // Save contact form data to database
+        ContactForm::create($request->all());
+
+        // Send email
+        Mail::to('info.isernepal@gmail.com')->send(new ContactFormMail($request->all()));
 
         return redirect()->back()->with('success', 'Thank you for your message. We will get back to you soon!');
     }
+
+
 }
---
+++ resources/views/emails/contact-form.blade.php
@@ -0,0 +1,7 @@
+@component('mail::message')
+**Name:** {{ $name }}
+
+**Email:** {{ $email }}
+
+**Message:** {{ $message }}
+@endcomponent
