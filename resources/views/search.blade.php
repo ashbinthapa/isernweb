833c6d9c24c5a537bb9a9b806d89354846b0a448
ashbinthapa <ashbinthapa2054@gmail.com>
2024-04-02 15:50:35 +0545

    search page build success for post: check remaining for other page

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -16,21 +16,30 @@
         </div>
     </div>
 </nav>
+
 <div class="container-fluid">
     <div class="container text-center" style="margin-top: 20px; margin-bottom: 20px;">
+        <h2 class="p-3" style="color: #01274c; margin-top: 20px; margin-bottom: 20px;">Search Results for
+            "{{ $query }}"</h2>
         <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
-            <h1>Search Results for "{{ $query }}"</h1>
 
             @if ($searchResults->isEmpty())
                 <p>No results found.</p>
             @else
-                <ul>
-                    @foreach ($searchResults as $result)
-                        <li>{{ $result->title }}</li>
-                        <!-- Assuming title is a common attribute among all searchable models -->
-                    @endforeach
-                </ul>
+                @foreach ($searchResults as $result)
+                    <div class="col">
+                        <div class="card h-100">
+                            <div class="card-body">
+                                <a href="{{ route('posts.show', ['slug' => $result->slug]) }}"
+                                    style="text-decoration: none; color: inherit;">
+                                    <h5 class="card-title">{{ $result->title }}</h5>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             @endif
+
         </div>
     </div>
 </div>
