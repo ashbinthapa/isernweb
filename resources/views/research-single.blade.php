2544c4ed848368d6cbcd542eab326b9b7a3bd464
ashbinthapa <ashbinthapa2054@gmail.com>
2024-04-18 12:32:51 +0545

    design part of research single

--- resources/views/research-single.blade.php
+++ resources/views/research-single.blade.php
@@ -1,33 +1,24 @@
 @include('layouts.header')
-
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">Home</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="/projects">Research</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link"
-                        href="/projects/{{ $data->first()->research_status }}">{{ $data->first()->research_status }}</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">{{ $data->title }}</a>
-                </li>
-                <!-- Add more navigation items as needed -->
-            </ul>
-        </div>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link fw-bold fs-4" style="color: #01274c;" href="/projects">Research</a>
+            </li>
+            <li class="nav-item p-3">
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+            </li>
+            <li class="nav-item pt-2">
+                <a class="nav-link fw-bold fs-6" style="color: #01274c;"
+                    href="/projects/{{ $data->first()->research_status }}">{{ $data->first()->research_status }}</a>
+            </li>
+            <!-- Add more navigation items as needed -->
+        </ul>
     </div>
 </nav>
 
 <div class="container my-5">
-    <h2 class="mb-4">{{ $data->title }}</h2>
-
+    <p class="mb-4 fw-bold fs-4">{{ $data->title }}</p>
     <div class="row">
         <div class="col-md-12"> <!-- Increased width to 9 columns -->
             <p class="lead">{!! $data->content !!}</p>
