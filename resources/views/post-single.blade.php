6ce236a57a20691bfe072ec077452ddd8822730b
ashbinthapa <ashbinthapa2054@gmail.com>
2024-04-18 11:48:08 +0545

    design part of post archive and post single updated

--- resources/views/post-single.blade.php
+++ resources/views/post-single.blade.php
@@ -1,28 +1,33 @@
 @include('layouts.header')
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">Home</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
-                </li>
-                <!-- Add more navigation items as needed -->
-            </ul>
-        </div>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link fw-bold fs-4" style="color: #01274c;" href="/posts">Posts</a>
+            </li>
+            <li class="nav-item p-3">
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+            </li>
+            <li class="nav-item pt-2">
+                <a class="nav-link fw-bold fs-6" style="color: #01274c;"
+                    href="/posts/category/{{ $post->category->name }}">{{ $post->category->name }}</a>
+            </li>
+            <!-- Add more navigation items as needed -->
+        </ul>
     </div>
 </nav>
 
+
 <div class="container my-5">
-    <h2 class="mb-4">{{ $post->title }}</h2>
+    <h2 class="mb-4 fw-bold fs-4">{{ $post->title }}</h2>
 
     <div class="row">
         <div class="col-md-12"> <!-- Increased width to 9 columns -->
-            <p class="lead">{!! $post->content !!}</p>
+            <p class="fs-6">{{ date('F j, Y', strtotime($post->updated_at)) }}
+            </p>
+        </div>
+        <div class="col-md-12"> <!-- Increased width to 9 columns -->
+            <p class="lead" style="font-size: 14px">{!! $post->content !!}</p>
         </div>
 
         <div class="col-md-3"> <!-- Decreased width to 3 columns -->
