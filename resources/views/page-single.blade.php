e0d9adbbbc419e99b019bc93b1b268aa6d686892
ashbinthapa <ashbinthapa2054@gmail.com>
2024-04-19 10:44:22 +0545

    design part of page single updated

--- resources/views/page-single.blade.php
+++ resources/views/page-single.blade.php
@@ -1,30 +1,27 @@
 @include('layouts.header')
-
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">Home</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                @if (isset($parentPage)) <!-- Check if parent page exists -->
-                    <li class="nav-item">
-                        <a class="nav-link" href="/{{ $parentPage->slug }}">{{ $parentPage->title }}</a>
-                    </li>
-                @endif
+        <ul class="navbar-nav">
+            @if (isset($parentPage))
+                <!-- Check if parent page exists -->
                 <li class="nav-item">
-                    <a class="nav-link">{{ $page->title }}</a>
+                    <a class="nav-link fw-bold fs-4" style="color: #01274c;"
+                        href="/{{ $parentPage->slug }}">{{ $parentPage->title }}</a>
                 </li>
-                <!-- Add more navigation items as needed -->
-            </ul>
-        </div>
+                <li class="nav-item p-3">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                </li>
+            @endif
+            <li class="nav-item pt-2">
+                <a class="nav-link fw-bold fs-6" style="color: #01274c;">{{ $page->title }}</a>
+            </li>
+            <!-- Add more navigation items as needed -->
+        </ul>
     </div>
 </nav>
 
 <div class="container my-5">
-    <h2 class="mb-4">{{ $page->title }}</h2>
+    <p class="mb-4 fw-bold fs-4">{{ $page->title }}</p>
 
     <div class="row">
         <div class="col-md-12"> <!-- Increased width to 9 columns -->
