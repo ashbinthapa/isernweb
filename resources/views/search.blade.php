253483e2d8ea83ddaee2bb45e911bc61827af0d0
ashbinthapa <ashbinthapa2054@gmail.com>
2024-04-23 13:54:34 +0545

    search page design updatedd

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,49 +1,45 @@
 @include('layouts.header')
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">Home</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Search Results</a>
-                </li>
-                <!-- Add more navigation items as needed -->
-            </ul>
-        </div>
+        <ul class="navbar-nav">
+            <li class="nav-item pt-2">
+                <a class="nav-link fw-bold fs-6" style="color: #01274c;">{{ $query }}</a>
+            </li>
+            <!-- Add more navigation items as needed -->
+        </ul>
     </div>
 </nav>
 
 <div class="container-fluid">
     <div class="container text-center" style="margin-top: 20px; margin-bottom: 20px;">
-        <h2 class="p-3" style="color: #01274c; margin-top: 20px; margin-bottom: 20px;">Search Results for
-            "{{ $query }}"</h2>
+        <p class="mb-4 fw-bold fs-4">Search Results for
+            "{{ $query }}"</p>
         <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
 
             @if ($searchResults->isEmpty())
                 <p>No results found.</p>
             @else
                 @foreach ($searchResults as $result)
-                    <div class="col">
+                    <div class="col p-2">
                         <div class="card h-100">
                             <div class="card-body">
                                 @if ($result->parent_slug != null && $result->child_slug != null)
                                     <a href="{{ route('page.single', ['parent_slug' => $result->parent_slug, 'child_slug' => $child_slug]) }}"
                                         style="text-decoration: none; color: inherit;">
-                                        <h5 class="card-title">{{ $result->title }}</h5>
+                                        <h5 class="card-title fw-bold" style="font-size: 16px;">
+                                            {{ $result->title }}</h5>
                                     </a>
                                 @elseif($result->research_status != null)
                                     <a href="{{ route('research.single', ['research_status' => $result->research_status, 'slug' => $result->slug]) }}"
                                         style="text-decoration: none; color: inherit;">
-                                        <h5 class="card-title">{{ $result->title }}</h5>
+                                        <h5 class="card-title fw-bold" style="font-size: 16px;">
+                                            {{ $result->title }}</h5>
                                     </a>
                                 @else
                                     <a href="{{ route('posts.show', ['slug' => $result->slug]) }}"
                                         style="text-decoration: none; color: inherit;">
-                                        <h5 class="card-title">{{ $result->title }}</h5>
+                                        <h5 class="card-title fw-bold" style="font-size: 16px;">
+                                            {{ $result->title }}</h5>
                                     </a>
                                 @endif
                             </div>
