c08eb44e4748465a619086587d5504253492ca25
ashbinthapa <ashbinthapa2054@gmail.com>
2024-04-26 14:40:25 +0545

    filament user updated second

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+
 
 class User extends Authenticatable implements FilamentUser
 {
