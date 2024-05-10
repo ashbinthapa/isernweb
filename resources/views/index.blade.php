e552179eaee4e49fc9c8f11ab1d6c46f5c685a36
ashbinthapa <ashbinthapa2054@gmail.com>
2024-05-10 12:27:33 +0545

    category of the news added in latest news and announcement section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -254,7 +254,7 @@
     <div class="container-fluid bg-light py-4">
         <div class="container text-center" style="margin-top: 20px; margin-bottom: 20px;">
             <p class="fw-bold fs-1" style="color: #01274c;">Latest News and
-                Announcements</p>
+                Announcement</p>
 
             <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
                 @foreach ($data['posts_latest_news'] as $post)
@@ -267,6 +267,8 @@
                             <div class="card-body">
                                 <p class="fs-6">{{ date('F j, Y', strtotime($post->updated_at)) }}
                                 </p>
+                                <span class="position-relative badge rounded-0 font-weight-bold"
+                                    style="background-color: #05264b;">{{ $post->category->name }}</span>
 
                                 <a href="{{ route('posts.show', ['slug' => $post->slug]) }}"
                                     style="text-decoration: none; color: inherit;">
