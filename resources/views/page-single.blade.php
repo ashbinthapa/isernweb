3db56346abf4678b5aa61366b6f4ad0471901995
ashbinthapa <ashbinthapa2054@gmail.com>
2024-05-31 13:43:18 +0545

    page navigation updated again

--- resources/views/page-single.blade.php
+++ resources/views/page-single.blade.php
@@ -3,6 +3,12 @@
     <div class="container">
         <p class="fw-bold fs-4 mt-4">{{ $page->title }}</p>
         <ul class="navbar-nav d-flex flex-row flex-wrap align-items-center">
+            <li class="nav-item">
+                <a class="nav-link fw-bold fs-4 fs-lg-4" style="color: #01274c;" href="{{ url('/') }}">Home</a>
+            </li>
+            <li class="nav-item">
+                <i class="fa fa-angle-right mx-2" aria-hidden="true"></i>
+            </li>
             @if (isset($parentPage))
                 <!-- Check if parent page exists -->
                 <li class="nav-item">
