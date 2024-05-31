c1bb36b626cdec5e09b4bd849f6b32b415dcc4fc
ashbinthapa <ashbinthapa2054@gmail.com>
2024-05-31 11:55:57 +0545

    footer design change

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,88 +1,93 @@
     <footer>
-        <div class="container-fluid p-5 custom-footer-style footer-details footer-design">
-            <div class="row m-5">
-                <div class="col-lg-3 col-md-6 col-sm-12">
-                    <h4 class="fw-bold">Contact Details</h4>
-                    <ul class="list-unstyled">
-                        <li><i class="fa fa-map" aria-hidden="true"></i>
-                            Bharatpur-15, Fulbari, Chitwan, Nepal</li>
-                        <li><i class="fa fa-phone" aria-hidden="true"></i>
-                            +977-56-591054 ,+977-56-592406</li>
-                        <li><i class="fa fa-envelope-o" aria-hidden="true"></i>
-                            iser.nepal@outlook.com</li>
-                        <li><i class="fa fa-clock-o" aria-hidden="true"></i>
-                            10 A.M. - 5 P.M.</li>
-                        <li>
-                            <a href="https://www.facebook.com/isernepalofficial/" target="_blank"
-                                class="social-icons-size">
-                                <i class="fa fa-facebook-square" aria-hidden="true"></i>
-                            </a>
-                            <a href="https://www.linkedin.com/in/iser-nepal-68b407160/" target="_blank"
-                                class="social-icons-size">
-                                <i class="fa fa-linkedin-square" aria-hidden="true"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-12">
-                    <h4 class="fw-bold">Forms</h4>
-                    <ul class="list-unstyled">
-                        <li><a href="#"> <i class="fa fa-link" aria-hidden="true"></i>
-                                Proposal Review Form</a></li>
-                        <li><a href="#"><i class="fa fa-link" aria-hidden="true"></i>
-                                Survey Data Analysis Training</a></li>
-                    </ul>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-12">
-                    <h4 class="fw-bold">Important Links</h4>
-                    <ul class="list-unstyled">
-                        <li><a href="https://cvfs.isr.umich.edu/"> <i class="fa fa-link" aria-hidden="true"></i>
-                                <strong>CVFS</strong>
-                                -Chitwan Valley Family Study</a></li>
-                        <li><a href="https://www.icpsr.umich.edu/web/ICPSR/search/studies?q=chitwan"> <i
-                                    class="fa fa-link" aria-hidden="true"></i>
-                                <strong>ICPSR</strong>
-                                -University of Michigan</a></li>
-                        <li><a href="https://ukdataservice.ac.uk/"> <i class="fa fa-link" aria-hidden="true"></i>
-                                <strong>UK Data Service</strong></a></li>
-                        <li><a href="http://idea.isernepal.org/"> <i class="fa fa-link" aria-hidden="true"></i>
-                                <strong>IDEA</strong></a></li>
-                        <li><a href="https://famelo.netlify.app/"> <i class="fa fa-link" aria-hidden="true"></i>
-                                <strong>FAMELO</strong></a></li>
-                        <li><a href="http://loaf.isernepal.org/"> <i class="fa fa-link" aria-hidden="true"></i>
-                                <strong>LOAF</strong></a></li>
-                        <li><a href="http://home.isr.umich.edu/"> <i class="fa fa-link" aria-hidden="true"></i>
-                                <strong>ISR</strong>
-                                -University of Michigan</a></li>
-                        <li><a href="https://censusnepal.cbs.gov.np/"> <i class="fa fa-link" aria-hidden="true"></i>
-                                <strong>CBS</strong>
-                                -Central Bureau of Statistics
-                            </a></li>
-                        <li><a href="https://bharatpurmun.gov.np/"> <i class="fa fa-link" aria-hidden="true"></i>
-                                Bharatpur Metropolitan City</a></li>
-                        <li><a href="https://nhrc.gov.np/"> <i class="fa fa-link" aria-hidden="true"></i>
-                                <strong>NHRC</strong>
-                                -Nepal Health Research Council</a></li>
-                        <li><a href="https://isernepal.org.np/other-links"> <i class="fa fa-link"
-                                    aria-hidden="true"></i>
-                                Other Links</a></li>
-                    </ul>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-12">
-                    <h4 class="fw-bold">Latest Events/News</h4>
-                    <div class="sh-recent-posts-widgets">
-                        @foreach ($posts_latest_news as $post)
-                            <div class="sh-recent-posts-widgets-item">
-                                <span class="post-meta-categories">
-                                    <a
-                                        href="/posts/category/{{ $post->category->name }}">{{ $post->category->name }}</a>
-                                </span>
-                                <a href="{{ route('posts.show', ['slug' => $post->slug]) }}">
-                                    <h6 class="fw-bold">{{ $post->title }}</h6>
-                                </a>
-                            </div>
-                        @endforeach
+        <div class="container-fluid custom-footer-style footer-details footer-design py-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 col-sm-12 p-3">
+                        <h5 class="fw-bold">Contact Details</h4>
+                            <ul class="list-unstyled">
+                                <li><i class="fa fa-map" aria-hidden="true"></i>
+                                    Bharatpur-15, Fulbari, Chitwan, Nepal</li>
+                                <li><i class="fa fa-phone" aria-hidden="true"></i>
+                                    +977-56-591054 ,+977-56-592406</li>
+                                <li><i class="fa fa-envelope-o" aria-hidden="true"></i>
+                                    iser.nepal@outlook.com</li>
+                                <li><i class="fa fa-clock-o" aria-hidden="true"></i>
+                                    10 A.M. - 5 P.M.</li>
+                                <li>
+                                    <a href="https://www.facebook.com/isernepalofficial/" target="_blank"
+                                        class="social-icons-size">
+                                        <i class="fa fa-facebook-square" aria-hidden="true"></i>
+                                    </a>
+                                    <a href="https://www.linkedin.com/in/iser-nepal-68b407160/" target="_blank"
+                                        class="social-icons-size">
+                                        <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-12 p-3">
+                        <h5 class="fw-bold">Forms</h4>
+                            <ul class="list-unstyled">
+                                <li><a href="#"> <i class="fa fa-link" aria-hidden="true"></i>
+                                        Proposal Review Form</a></li>
+                                <li><a href="#"><i class="fa fa-link" aria-hidden="true"></i>
+                                        Survey Data Analysis Training</a></li>
+                            </ul>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-12 p-3">
+                        <h5 class="fw-bold">Important Links</h4>
+                            <ul class="list-unstyled">
+                                <li><a href="https://cvfs.isr.umich.edu/"> <i class="fa fa-link" aria-hidden="true"></i>
+                                        <strong>CVFS</strong>
+                                        -Chitwan Valley Family Study</a></li>
+                                <li><a href="https://www.icpsr.umich.edu/web/ICPSR/search/studies?q=chitwan"> <i
+                                            class="fa fa-link" aria-hidden="true"></i>
+                                        <strong>ICPSR</strong>
+                                        -University of Michigan</a></li>
+                                <li><a href="https://ukdataservice.ac.uk/"> <i class="fa fa-link"
+                                            aria-hidden="true"></i>
+                                        <strong>UK Data Service</strong></a></li>
+                                <li><a href="http://idea.isernepal.org/"> <i class="fa fa-link" aria-hidden="true"></i>
+                                        <strong>IDEA</strong></a></li>
+                                <li><a href="https://famelo.netlify.app/"> <i class="fa fa-link" aria-hidden="true"></i>
+                                        <strong>FAMELO</strong></a></li>
+                                <li><a href="http://loaf.isernepal.org/"> <i class="fa fa-link" aria-hidden="true"></i>
+                                        <strong>LOAF</strong></a></li>
+                                <li><a href="http://home.isr.umich.edu/"> <i class="fa fa-link" aria-hidden="true"></i>
+                                        <strong>ISR</strong>
+                                        -University of Michigan</a></li>
+                                <li><a href="https://censusnepal.cbs.gov.np/"> <i class="fa fa-link"
+                                            aria-hidden="true"></i>
+                                        <strong>CBS</strong>
+                                        -Central Bureau of Statistics
+                                    </a></li>
+                                <li><a href="https://bharatpurmun.gov.np/"> <i class="fa fa-link"
+                                            aria-hidden="true"></i>
+                                        Bharatpur Metropolitan City</a></li>
+                                <li><a href="https://nhrc.gov.np/"> <i class="fa fa-link" aria-hidden="true"></i>
+                                        <strong>NHRC</strong>
+                                        -Nepal Health Research Council</a></li>
+                                <li><a href="https://isernepal.org.np/other-links"> <i class="fa fa-link"
+                                            aria-hidden="true"></i>
+                                        Other Links</a></li>
+                            </ul>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-12 p-3">
+                        <h5 class="fw-bold">Latest Events/News</h4>
+                            <div class="sh-recent-posts-widgets">
+                                @foreach ($posts_latest_news as $post)
+                                    <div class="sh-recent-posts-widgets-item">
+                                        <span class="post-meta-categories">
+                                            <a
+                                                href="/posts/category/{{ $post->category->name }}">{{ $post->category->name }}</a>
+                                        </span>
+                                        <a href="{{ route('posts.show', ['slug' => $post->slug]) }}">
+                                            <h6 class="fw-bold">{{ $post->title }}</h6>
+                                        </a>
+                                    </div>
+                                @endforeach
 
+                            </div>
                     </div>
                 </div>
             </div>
