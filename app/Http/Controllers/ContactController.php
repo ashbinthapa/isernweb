996db1715da8e6f52730daf003fd60f2b8d39868
ashbinthapa <ashbinthapa2054@gmail.com>
2024-06-13 10:43:58 +0545

    sanatizing the contact form data to ensure the security

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -23,19 +23,21 @@ class ContactController extends Controller
             'message' => 'nullable',
         ]);
 
-        /*this block of code can be replaced by */
-        /*$contact = new ContactForm();
-        $contact->name = $request->name;
-        $contact->email = $request->email;
-        $contact->message = $request->message;
-        $contact->save();
-        */    
-        /*replaced by this */
-        // Save contact form data to database
-        ContactForm::create($request->all());
+        // Sanitize input
+        $sanitizedData = $request->only(['name', 'email', 'message']);
+        $sanitizedData['name'] = htmlspecialchars($sanitizedData['name'], ENT_QUOTES, 'UTF-8');
+        $sanitizedData['email'] = htmlspecialchars($sanitizedData['email'], ENT_QUOTES, 'UTF-8');
+        $sanitizedData['message'] = htmlspecialchars($sanitizedData['message'], ENT_QUOTES, 'UTF-8');
 
-        // Send email
-        Mail::to('info.isernepal@gmail.com')->send(new ContactFormMail($request->all()));
+        // Save contact form data to database
+        ContactForm::create($sanitizedData);
+
+        try {
+            // Send email
+            Mail::to('info.isernepal@gmail.com')->send(new ContactFormMail($sanitizedData));
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'There was an issue sending your message. Please try again later.');
+        }
 
         return redirect()->back()->with('success', 'Thank you for your message. We will get back to you soon!');
     }
